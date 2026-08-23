9e71566df6a3bbb37838a39147e2459fb37f9c3b
KobayashiTetsuya <qtetsu.8201.tetsuq@gmail.com>
2026-08-24 02:11:33 +0900

    [update]イベントマスの演出追加とタップで進めれるようにした

--- Assets/_Scripts/_StageSelectScene/Event/EventChoiceButton.cs
+++ Assets/_Scripts/_StageSelectScene/Event/EventChoiceButton.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,6 +7,9 @@ public class EventChoiceButton : MonoBehaviour
 {
     [SerializeField, Tooltip("選択肢のボタン")] private Button _button;
     [SerializeField, Tooltip("選択肢のテキスト")] private TMP_Text _choiceText;
+    [SerializeField] private Image _img;
+    [SerializeField] private float _duration = 0.2f;
+    [SerializeField] private float _textDuration = 0.3f;
 
     private EventChoice _eventChoice;
     private EventPanelController _controller;
@@ -14,9 +18,27 @@ public class EventChoiceButton : MonoBehaviour
     {
         _eventChoice = choice;
         _controller = controller;
-        _choiceText.text = choice.ChoiceText;
+        int length = 0;
+        string text = choice.ChoiceText;
+        _choiceText.text = "";
         _button.onClick.RemoveAllListeners();
         _button.onClick.AddListener(OnClick);
+        _img.color = new Color(1f, 1f, 1f, 0f);
+        Sequence seq = DOTween.Sequence();
+        seq.AppendInterval(0.8f);
+        seq.Append(_img.DOFade(1f, _duration));
+        seq.AppendCallback(() =>
+        {
+            DOTween.To(() => length,
+                    x =>
+                    {
+                        length = x;
+                        _choiceText.text = text.Substring(0, length);
+
+                    },
+                    text.Length,
+                    _textDuration);
+        });
     }
 
     private void OnClick()
